bd3ab1b288904d62a1753aec67b0ce5a6fefcda5
Thiago <thiagowirzdemoura@gmail.com>
2022-02-04 09:14:50 -0300

    OutputPage logic done, sema as inputPage

--- src/Pages/OutputPage/index.js
+++ src/Pages/OutputPage/index.js
@@ -1,23 +1,73 @@
 import { Container, Button } from "../InputPage/style";
+import { useNavigate } from "react-router-dom";
+import UserContext from "../../contexts/userContext";
+import { postCredit } from "../../services/mywallet";
+import { useState, useContext } from "react";
+import Loader from "react-loader-spinner";
 
-export default function OutputPage(){
-    return(
-        <Container>
-            <header>Nova saída</header>
-          <form>
-            <input
-              type="number"
-              placeholder="Valor"
-              name="amount"
+export default function OutputPage() {
+  const [formData, setFormData] = useState({
+    value: "",
+    description: "",
+    type: "output",
+  });
+  const [loading, setLoading] = useState(false);
+  const { user } = useContext(UserContext);
+  const config = { headers: { Authorization: `Bearer ${user.token}` } };
+  const navigate = useNavigate();
+
+  function handleInputChange(e) {
+    formData[e.target.name] = e.target.value;
+    setFormData({ ...formData });
+  }
+
+  function handlePostCredit(e) {
+    e.preventDefault();
+    setLoading(true);
+    const promise = postCredit(formData, config);
+    promise.then(() => {
+      setLoading(false);
+      navigate("/account");
+    });
+    promise.catch((error) => {
+      alert(error.response.data.message);
+      setLoading(false);
+    });
+  }
+
+  return (
+    <Container>
+      <header>Nova saída</header>
+      <form onSubmit={handlePostCredit}>
+        <input
+          disabled={loading}
+          onChange={handleInputChange}
+          value={formData.value}
+          type="number"
+          placeholder="Valor"
+          name="value"
+        />
+        <input
+          disabled={loading}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Descrição"
+          name="description"
+        />
+        <Button disabled={loading}>
+          {loading ? (
+            <Loader
+              type="ThreeDots"
+              color="#FFFFFF"
+              height={13}
+              width={51}
+              timeout={3000}
             />
-            <input
-              type="text"
-              placeholder="Descrição"
-              name="description"
-            />
-            <Button>Salvar saída
-            </Button>
-          </form>
-        </Container>
-    )
+          ) : (
+            "Salvar Saída"
+          )}
+        </Button>
+      </form>
+    </Container>
+  );
 }
